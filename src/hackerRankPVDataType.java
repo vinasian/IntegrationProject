570ca1bc53c45df910c76fa2c2d6b8bd13455baa
vinasian <43689873+vinasian@users.noreply.github.com>
2019-02-05 00:12:27 -0500

    push integration

---
+++ src/hackerRankPVDataType.java
@@ -0,0 +1,13 @@
+
+public class hackerRankPVDataType {
+
+    public static void main(String[] args) {
+      int int1 = 2;
+      double double1 = 2.0;
+      String string1 = "2";
+        
+      System.out.println("The result of + with ints is " + (int1+int1));
+      System.out.println("The result of + with doubles is " + (double1 + double1));
+      System.out.println("The result of + with Strings is " + (string1 + string1));
+    }
+}
---
+++ src/helloWorldJav.java
@@ -0,0 +1,9 @@
+
+public class helloWorldJav {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello worlds!");	
+	}
+
+}
